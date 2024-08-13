a67f849232432bfb55bb8e891676fc84864c696d
aminur-sarker <aminursarker2002@gmail.com>
2024-08-14 02:49:15 +0600

    password error handle if length< 6 char

--- src/components/SimpleForm/SimpleForm.jsx
+++ src/components/SimpleForm/SimpleForm.jsx
@@ -4,9 +4,15 @@ const SimpleForm = () => {
     const [name, setName]=useState('')
     const [email, setEmail]=useState('')
     const [password, setPassword]=useState('')
+    const [error, setError] = useState('')
 
     const handleSubmit = e =>{
         e.preventDefault();
+        if(password.length<6){
+            setError('Password must be 6 character or longer')
+        }else{
+            setError('')
+        }
         console.log(e.target.name.value) 
         console.log(name,email, password) 
     }
@@ -20,6 +26,7 @@ const SimpleForm = () => {
     }
 
     const handlePasswordChange= e =>{
+        if(password.length<6)
         setPassword(e.target.value)
     }
 
@@ -37,6 +44,10 @@ const SimpleForm = () => {
                 <br/>
 
                 <input type="submit" value="Submit" />
+
+                {
+                    error && <p>{error}</p>
+                }
             </form>
         </div>
     );
